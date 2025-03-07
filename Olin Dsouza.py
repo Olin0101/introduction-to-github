bb04a644f937baf3a46b705263ff5ca14706de02
Olin0101 <86824445+Olin0101@users.noreply.github.com>
2025-03-06 23:45:22 -0500

    Add files via upload

---
+++ Olin Dsouza.py
@@ -0,0 +1,7 @@
+# fullName.py
+# Author: Olin Dsouza
+# Date: 02/07/2025
+# Description: A Simple python program to  Ask for the first and last name, then print them in "Last, First" format.
+first_name = input("Please enter your first name: ")#  to input their first name
+last_name = input("Please enter your last name: ")#  to input their last name
+print(f"{last_name}, {first_name}")# Output the user's name in last name, first name format
