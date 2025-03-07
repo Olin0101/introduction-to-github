2a4bb02cd8a90f4735050e5f97267e36e8aad177
Olin0101 <86824445+Olin0101@users.noreply.github.com>
2025-03-06 23:50:07 -0500

    Add files via upload
    line  slope

---
+++ lineSlope.py
@@ -0,0 +1,21 @@
+ # Author: Olin Dsouza
+# Date: 02/07/2025
+# Description: This program computes the slope of a line given 
+# the end points of the line. The result is then printed to the screen.
+
+# Initialize the end points.
+startX = -2
+startY = 1
+endX = 5
+endY = 36
+
+# Compute the slope.
+slope = (startY - endY) / (startX - endX)
+
+# Print the results.
+print("Starting point: (", startX, ",", startY, ")")
+print("Ending point: (", endX, ",", endY, ")")
+print("Slope of the line =", slope)
+
+
+
